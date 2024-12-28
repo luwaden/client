1939ea345707cf6de312ae38c220c245ebdba485
Ayodeji Dennis <iamluwaden@gmail.com>
2024-12-28 17:39:07 +0100

    mapping products

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 
 import "./App.css";
+import { sampleProducts } from "./data";
 
 function App() {
   return (
     <div className='App'>
       <h1>prox commerce</h1>
-      <h3>client side</h3>
+      <main>
+        <ul>
+          {sampleProducts.map((product) => (
+            <li key={product.slug}>
+              <img
+                src={product.image}
+                alt={product.name}
+                className='product-image'
+              />
+              <h2>{product.name}</h2>
+              <p>${product.price}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
     </div>
   );
 }
